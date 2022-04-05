9a499bc80cad40579960d1b6749e5afc48c8e1e6
twinaphex <libretro@gmail.com>
2022-04-06 00:28:01 +0200

    Cleanups

--- Core/MemoryAccessCounter.cpp
+++ Core/MemoryAccessCounter.cpp
@@ -40,77 +40,6 @@ MemoryAccessCounter::MemoryAccessCounter(Debugger* debugger)
 	}
 }
 
-bool MemoryAccessCounter::IsAddressUninitialized(AddressTypeInfo &addressInfo)
-{
-	if(addressInfo.Type == AddressType::InternalRam || addressInfo.Type == AddressType::WorkRam) {
-		return _counters[(int)addressInfo.Type][addressInfo.Address].WriteCount == 0;
-	}
-	return false;
-}
-
-void MemoryAccessCounter::ProcessPpuMemoryRead(PpuAddressTypeInfo &addressInfo, uint64_t cpuCycle)
-{
-	if(addressInfo.Address < 0) {
-		return;
-	}
-
-	AddressCounters& counts = _ppuCounters[(int)addressInfo.Type][addressInfo.Address];
-	counts.ReadCount++;
-	counts.ReadStamp = cpuCycle;
-}
-
-void MemoryAccessCounter::ProcessPpuMemoryWrite(PpuAddressTypeInfo& addressInfo, uint64_t cpuCycle)
-{
-	if(addressInfo.Address < 0) {
-		return;
-	}
-
-	AddressCounters& counts = _ppuCounters[(int)addressInfo.Type][addressInfo.Address];
-	counts.WriteCount++;
-	counts.WriteStamp = cpuCycle;
-}
-
-bool MemoryAccessCounter::ProcessMemoryRead(AddressTypeInfo &addressInfo, uint64_t cpuCycle)
-{
-	if(addressInfo.Address < 0) {
-		return false;
-	}
-
-	AddressCounters& counts = _counters[(int)addressInfo.Type][addressInfo.Address];
-	counts.ReadCount++;
-	counts.ReadStamp = cpuCycle;
-
-	if(counts.WriteCount == 0 && (addressInfo.Type == AddressType::InternalRam || addressInfo.Type == AddressType::WorkRam)) {
-		//Mark address as read before being written to (if trying to read/execute)
-		counts.UninitRead = true;
-		return true;
-	}
-
-	return false;
-}
-
-void MemoryAccessCounter::ProcessMemoryWrite(AddressTypeInfo& addressInfo, uint64_t cpuCycle)
-{
-	if(addressInfo.Address < 0) {
-		return;
-	}
-
-	AddressCounters& counts = _counters[(int)addressInfo.Type][addressInfo.Address];
-	counts.WriteCount++;
-	counts.WriteStamp = cpuCycle;
-}
-
-void MemoryAccessCounter::ProcessMemoryExec(AddressTypeInfo& addressInfo, uint64_t cpuCycle)
-{
-	if(addressInfo.Address < 0) {
-		return;
-	}
-	
-	AddressCounters& counts = _counters[(int)addressInfo.Type][addressInfo.Address];
-	counts.ExecCount++;
-	counts.ExecStamp = cpuCycle;
-}
-
 void MemoryAccessCounter::ResetCounts()
 {
 	for(int i = 0; i < 4; i++) {
@@ -118,25 +47,6 @@ void MemoryAccessCounter::ResetCounts()
 			_counters[i][j] = { (uint32_t)j };
 		for(int j = 0; j < _ppuCounters[i].size(); j++)
 			_ppuCounters[i][j] = { (uint32_t)j };
-	}
-}
-
-void MemoryAccessCounter::GetNametableChangedData(bool ntChangedData[])
-{
-	PpuAddressTypeInfo addressInfo;
-	uint64_t cpuCycle = _debugger->GetConsole()->GetCpu()->GetCycleCount();
-	NesModel model = _debugger->GetConsole()->GetModel();
-	double frameRate = model == NesModel::NTSC ? 60.1 : 50.01;
-	double overclockRate = _debugger->GetConsole()->GetPpu()->GetOverclockRate() * 100;
-	uint32_t cyclesPerFrame = (uint32_t)(_debugger->GetConsole()->GetCpu()->GetClockRate(model) / frameRate * overclockRate);
-
-	for(int i = 0; i < 0x1000; i++) {
-		_debugger->GetPpuAbsoluteAddressAndType(0x2000+i, &addressInfo);
-		if(addressInfo.Type != PpuAddressType::None) {
-			ntChangedData[i] = (cpuCycle - _ppuCounters[(int)addressInfo.Type][addressInfo.Address].WriteStamp) < cyclesPerFrame;
-		} else {
-			ntChangedData[i] = false;
-		}
 	}
 }
 
--- Core/MemoryAccessCounter.h
+++ Core/MemoryAccessCounter.h
@@ -28,16 +28,7 @@ private:
 public:
 	MemoryAccessCounter(Debugger* debugger);
 
-	void ProcessPpuMemoryRead(PpuAddressTypeInfo& addressInfo, uint64_t cpuCycle);
-	void ProcessPpuMemoryWrite(PpuAddressTypeInfo &addressInfo, uint64_t cpuCycle);
-	bool ProcessMemoryRead(AddressTypeInfo& addressInfo, uint64_t cpuCycle);
-	void ProcessMemoryWrite(AddressTypeInfo& addressInfo, uint64_t cpuCycle);
-	void ProcessMemoryExec(AddressTypeInfo &addressInfo, uint64_t cpuCycle);
-
 	void ResetCounts();
 
-	bool IsAddressUninitialized(AddressTypeInfo &addressInfo);
-	
-	void GetNametableChangedData(bool ntChangedData[]);
 	void GetAccessCounts(uint32_t offset, uint32_t length, DebugMemoryType memoryType, AddressCounters counts[]);
 };
