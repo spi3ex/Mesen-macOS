fe9d2d3302826076d518aaf8c85f20cc9e9e3605
vailkyte <133282358+vailkyte@users.noreply.github.com>
2023-05-18 07:59:12 -0500

    Backport FDS - Fixed incorrect audio pitch in Nazoraa Land Dai 3 Gou

--- Core/BaseFdsChannel.h
+++ Core/BaseFdsChannel.h
@@ -13,8 +13,8 @@ protected:
 
 	uint32_t _timer = 0;
 
-	//"Few FDS NSFs write to this register. The BIOS initializes this to $FF."
-	uint8_t _masterSpeed = 0xFF;
+	//"Few FDS NSFs write to this register. The BIOS initializes this to $E8."
+	uint8_t _masterSpeed = 0xE8;
 
 	void StreamState(bool saving) override
 	{
@@ -71,15 +71,35 @@ public:
 		}
 		return false;
 	}
+	
+	uint8_t GetSpeed()
+	{
+		return _speed;
+	}
 
 	uint8_t GetGain()
 	{
 		return _gain;
 	}
 
+	uint8_t GetMasterSpeed()
+	{
+		return _masterSpeed;
+	}
+
 	uint16_t GetFrequency()
 	{
 		return _frequency;
+	}
+
+	bool GetVolumeIncreaseFlag()
+	{
+		return _volumeIncrease;
+	}
+
+	bool IsEnvelopeDisabled()
+	{
+		return _envelopeOff;
 	}
 
 	void ResetTimer()
